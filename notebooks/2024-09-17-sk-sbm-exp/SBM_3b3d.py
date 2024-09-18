e32c33a59dd0ab0564454b0458aacc3a5b1f4266
Sadamori Kojaku <skojaku@bingamton.edu>
2024-09-18 06:34:10 -0400

    update the figures

---
+++ notebooks/2024-09-17-sk-sbm-exp/SBM_3b3d.py
@@ -0,0 +1,196 @@
+# %% [markdown]
+# # Matrix weight
+# This is to explore the higher characteristic dimension.
+
+import numpy as np
+import pandas as pd
+import igraph as ig
+import networkx as nx
+import utils
+from scipy import sparse
+from mpl_toolkits import mplot3d
+from scipy.sparse.linalg import expm
+import sys
+
+# ---------------------------------
+# Generate the matrix-weighted SBM
+# ---------------------------------
+if "snakemake" in sys.modules:
+    n_nodes = int(snakemake.params["n_nodes"])
+    n_communities = int(snakemake.params["n_communities"])
+    pin = float(snakemake.params["pin"])
+    pout = float(snakemake.params["pout"])
+    dim = int(snakemake.params["dim"])
+    noise = float(snakemake.params["noise"])
+    coherence = float(snakemake.params["coherence"])
+    output_file = snakemake.output["output_file"]
+else:
+    n_nodes = 90
+    n_communities = 3
+    pin, pout = 0.3, 0.3
+    dim = 5
+    noise = 0.0
+    coherence = 1.0
+    output_file = "test_fig.pdf"
+
+A_mat, A, membership, com_com_rotation_matrix = utils.generate_matrix_weighted_sbm(
+    n_nodes=n_nodes,
+    n_communities=n_communities,
+    pin=pin,
+    pout=pout,
+    coherence=coherence,
+    noise=noise,
+    dim=dim,
+)
+
+# ---------------------------------
+# Initialzie node characteristics
+# ---------------------------------
+# Sample non-zero characteristic nodes from the first community
+non_zero_characteristic_nodes = np.random.choice(n_nodes, n_nodes // n_communities)
+y_0 = utils.generate_initial_characteristic_vector(
+    nodes=non_zero_characteristic_nodes, n_nodes=n_nodes, dim=dim
+)
+
+
+# ---------------------------------
+# Consensus Dynamics
+# ---------------------------------
+# Theoretical results
+y_star = utils.calc_theoretical_results(
+    y_0=y_0,
+    focal_node=0,
+    membership=membership,
+    coherence=coherence,
+    noise=noise,
+    com_com_rotation_matrix=com_com_rotation_matrix,
+    n_nodes=n_nodes,
+    n_communities=n_communities,
+    dim=dim,
+)
+
+# Numerical results
+degree = np.array(A.sum(axis=1)).reshape(-1)
+D_mat = sparse.diags(np.repeat(degree, dim))
+L_mat = D_mat - A_mat  # Laplacian matrix
+
+ts = [0.01, 0.05, 0.1, 0.5, 1, 5, 10, 20, 50]
+res = [(expm(-L_mat * t) @ y_0).toarray().reshape(n_nodes, dim) for t in ts]
+node_states = np.swapaxes(np.array(res), 0, 1)  # shape = (node x time x dim)
+node_states.shape
+# %% Identify the principal components of the node states
+from sklearn.decomposition import PCA
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+X = np.vstack([node_states[:, t, :] for t in range(node_states.shape[1])])
+pca = PCA(n_components=2).fit(X)
+xy = pca.transform(X)
+node_states_2d = np.array(
+    [pca.transform(node_states[:, t, :]) for t in range(node_states.shape[1])]
+)
+node_states_2d = np.swapaxes(node_states_2d, 0, 1)
+
+y_star_2d = pca.transform(y_star)
+
+plot_data = pd.DataFrame(
+    {
+        "x": [node_states_2d[i, t, 0] for i in range(n_nodes) for t in range(len(ts))],
+        "y": [node_states_2d[i, t, 1] for i in range(n_nodes) for t in range(len(ts))],
+        "node_id": np.repeat(range(n_nodes), len(ts)),
+        "t": np.tile(ts, n_nodes),
+        "membership": np.repeat(membership, len(ts)),
+    }
+)
+# %%
+sns.set_style("white")
+sns.set(font_scale=1.2)
+sns.set_style("ticks")
+
+fig, ax = plt.subplots(figsize=(6, 6))
+
+# Sample the same number of nodes for each community
+sample_node_size = 10
+sampled_nodes = []
+for c in range(n_communities):
+    sampled_nodes.extend(
+        np.random.choice(
+            np.where(membership == c)[0], size=sample_node_size, replace=False
+        )
+    )
+
+#  Add arrows from each node at t to the corresponding node at t+1
+for t in range(len(ts) - 1):
+    for i in sampled_nodes:
+        dx = node_states_2d[i, t + 1, 0] - node_states_2d[i, t, 0]
+        dy = node_states_2d[i, t + 1, 1] - node_states_2d[i, t, 1]
+        arrow_length = np.sqrt(dx**2 + dy**2)
+        head_length = min(0.02, arrow_length * 0.15)  # Reduced head length
+        head_width = head_length * 0.8  # Adjusted head width
+
+        # Calculate the margin to avoid overlap
+        margin = 0.015  # Adjust this value to increase/decrease the margin
+        start_x = node_states_2d[i, t, 0] + margin * dx / arrow_length
+        start_y = node_states_2d[i, t, 1] + margin * dy / arrow_length
+        end_x = node_states_2d[i, t + 1, 0] - margin * dx / arrow_length
+        end_y = node_states_2d[i, t + 1, 1] - margin * dy / arrow_length
+
+        ax.annotate(
+            "",
+            xy=(end_x, end_y),
+            xytext=(start_x, start_y),
+            arrowprops=dict(
+                arrowstyle="->",
+                color="#4d4d4d",
+                alpha=0.5,
+                shrinkA=0,
+                shrinkB=0,
+                mutation_scale=10,  # Controls the size of the arrow head
+                linestyle="dashed",  # Add dashed line style
+            ),
+        )
+
+cmap_nodes = sns.color_palette(desat=0.3)
+cmap_star = sns.color_palette("bright")
+
+cmap_nodes[2] = cmap_nodes[1]
+cmap_nodes[0] = cmap_nodes[-1]
+cmap_nodes[1] = cmap_nodes[-4]
+cmap_star[2] = cmap_star[1]
+cmap_star[0] = cmap_star[-1]
+cmap_star[1] = cmap_star[-4]
+
+ax = sns.scatterplot(
+    data=plot_data.query("node_id in @sampled_nodes"),
+    x="x",
+    y="y",
+    hue="membership",
+    palette=cmap_nodes[:n_communities],
+    ax=ax,
+    style="membership",
+    markers=["o", "s", "D", "X"],
+    edgecolor="#6d6d6d",
+    s=50,
+    zorder=10,
+)
+
+for i in range(n_communities):
+    ax.scatter(
+        y_star_2d[i, 0],
+        y_star_2d[i, 1],
+        marker="*",
+        color=cmap_star[i],
+        s=500,
+        zorder=100,
+        edgecolor="k",
+    )
+
+ax.set_xlabel("PC 1")
+ax.set_ylabel("PC 2")
+ax.set_title("Consensus Dynamics")
+ax.legend(title="Community", loc="upper right", fontsize=12, frameon=False)
+sns.despine()
+
+fig.savefig(output_file, bbox_inches="tight")
+
+# %%
---
+++ notebooks/2024-09-17-sk-sbm-exp/utils.py
@@ -0,0 +1,300 @@
+import numpy as np
+from itertools import combinations
+import networkx as nx
+from scipy import sparse
+
+
+def generate_random_rotation_matrix(k):
+    """
+    Generate a random rotation matrix.
+
+    Parameters
+    ----------
+    k: int
+        Dimension of the matrix-weighted SBM.
+
+    Returns
+    -------
+    A: numpy.ndarray
+        Random rotation matrix.
+
+    """
+    A = np.eye(k)
+    planes = list(combinations(range(k), 2))
+    np.random.shuffle(planes)
+    for i, j in planes:
+        val = np.random.uniform(-1, 1)
+        th = val * np.pi
+        # print("planes: ({}, {}), angle: {} pi".format(i, j, val))
+        R = np.eye(k)
+        R[i, i] = np.cos(th)
+        R[i, j] = -np.sin(th)
+        R[j, i] = np.sin(th)
+        R[j, j] = np.cos(th)
+        A = R @ A
+    return A
+
+
+def generate_rotation_noise_matrix(k, noise=0.0):
+    """
+    Generate a rotation matrix with noise.
+
+    Parameters
+    ----------
+    k: int
+        Dimension of the matrix-weighted SBM.
+    noise: float
+        Noise parameter.
+
+    Returns
+    -------
+    A: numpy.ndarray
+        Rotation matrix with noise.
+
+    """
+    A = np.eye(k)
+    planes = list(combinations(range(k), 2))
+    np.random.shuffle(planes)
+    for i, j in planes:
+        val = np.random.normal(scale=noise)
+        th = val * np.pi
+        # print("planes: ({}, {}), noise angle: {} pi".format(i, j, val))
+        R = np.eye(k)
+        R[i, i] = np.cos(th)
+        R[i, j] = -np.sin(th)
+        R[j, i] = np.sin(th)
+        R[j, j] = np.cos(th)
+        A = R @ A
+    return A
+
+
+def assign_rotation_matrix(
+    src, trg, membership, com_com_rotation_matrix, coherence, noise, dim
+):
+    """
+    Assign a rotation matrix to an edge.
+
+    Parameters
+    ----------
+    src: int
+        Source node.
+    trg: int
+        Target node.
+    membership: numpy.ndarray
+        Community assignment of each node.
+    com_com_rotation_matrix: dict
+        Rotation matrix for each pair of communities.
+    coherence: float
+        Coherence parameter.
+    noise: float
+        Noise parameter.
+    dim: int
+        Dimension of the matrix-weighted SBM.
+
+    Returns
+    -------
+    rotation_matrix: numpy.ndarray
+        Rotation matrix for the edge.
+
+    """
+    k, l = membership[src], membership[trg]
+    if np.random.rand() < coherence:
+        return com_com_rotation_matrix[
+            (k, l)
+        ]  # assign the community to community matrix
+    else:
+        return generate_rotation_noise_matrix(k=dim, noise=noise).dot(
+            com_com_rotation_matrix[(k, l)]
+        )
+
+
+def generate_matrix_weighted_sbm(
+    n_nodes,
+    n_communities,
+    pin,
+    pout,
+    coherence,
+    noise,
+    dim,
+):
+    """
+    Generate a matrix-weighted SBM.
+
+    Parameters
+    ----------
+    n_nodes: int
+        Number of nodes.
+    n_communities: int
+        Number of communities.
+    pin: float
+        Probability of intra-community edges.
+    pout: float
+        Probability of inter-community edges.
+    coherence: float
+        Coherence parameter.
+    noise: float
+        Noise parameter.
+    dim: int
+        Dimension of the matrix-weighted SBM.
+
+    Returns
+    -------
+    A_mat: scipy.sparse.csr_matrix
+        Matrix-weighted SBM.
+    A: scipy.sparse.csr_matrix
+        Base network
+    membership: numpy.ndarray
+        Community assignment of each node.
+    com_com_rotation_matrix: dict
+        Rotation matrix for each pair of communities.
+    """
+    assert n_nodes % n_communities == 0, "n_nodes must be divisible by n_communities"
+
+    # Generate a base network using the stochastic block model
+    pref_matrix = np.full((n_communities, n_communities), pout)
+    pref_matrix[np.diag_indices_from(pref_matrix)] = pin
+    block_sizes = [n_nodes // n_communities] * n_communities
+
+    g = nx.stochastic_block_model(block_sizes, pref_matrix, seed=0, directed=False)
+    A = nx.to_scipy_sparse_array(g)
+
+    src, trg, _ = sparse.find(sparse.triu(A, 1))  # only edges
+    membership = np.digitize(np.arange(n_nodes), np.cumsum(block_sizes))
+
+    # intially balanced configuration
+    # internal edges - identity matrix
+    com_com_rotation_matrix = {(l, l): np.eye(dim) for l in range(n_communities)}
+    # external edges - first path random
+    com_com_rotation_matrix.update(
+        {
+            (l, k): generate_random_rotation_matrix(k=dim)
+            for l in range(n_communities)
+            for k in range(l + 1, n_communities)
+        }
+    )
+    com_com_rotation_matrix.update(
+        {
+            (k, l): com_com_rotation_matrix[(l, k)].T
+            for l in range(n_communities)
+            for k in range(l + 1, n_communities)
+        }
+    )
+    # external edges - others by existing ones
+    # if n_communities == 2:
+    #    com_com_rotation_matrix[(1, 0)] = com_com_rotation_matrix[0, 1].T
+
+    # construct the block weight matrix
+    matrix_blocks = [[None for _ in range(n_nodes)] for _ in range(n_nodes)]
+
+    for k, l in zip(src, trg):
+        rot = assign_rotation_matrix(
+            k, l, membership, com_com_rotation_matrix, coherence, noise, dim
+        )
+        matrix_blocks[k][l] = rot
+        matrix_blocks[l][k] = rot.T
+
+    A_mat = sparse.bmat(matrix_blocks, format="csr")
+    return A_mat, A, membership, com_com_rotation_matrix
+
+
+def generate_initial_characteristic_vector(nodes, n_nodes, dim):
+    """
+    Generate an initial characteristic vector.
+
+    Parameters
+    ----------
+    nodes: numpy.ndarray
+        Nodes to be assigned the characteristic vector.
+    n_nodes: int
+        Number of nodes.
+    dim: int
+        Dimension of the matrix-weighted SBM.
+
+    Returns
+    -------
+    y_0: scipy.sparse.csc_matrix
+        Initial characteristic vector.
+    """
+    # inital characteristic vector for nodes
+    # s1 - choose a random position, and assign to nodes in one community
+    n_sel = len(nodes)
+
+    # y_0 is a concatenation of node vectors of length dim * n_nodes.
+    # A node's vector is given by the following construction.
+    # y_0[node * dim:(node+1) * dim]
+    col = np.zeros(n_sel * dim)
+    row = np.kron(nodes * dim, np.ones(dim))
+    row += np.kron(np.ones(n_sel), np.arange(dim))
+
+    y_0_2d = np.random.rand(dim)
+    y_0_2d /= np.linalg.norm(y_0_2d)
+    data = np.kron(np.ones(n_sel), y_0_2d)
+    y_0 = sparse.csc_matrix((data, (row, col)), shape=(n_nodes * dim, 1))
+    return y_0
+
+
+def calc_theoretical_results(
+    y_0,
+    focal_node,
+    membership,
+    coherence,
+    noise,
+    com_com_rotation_matrix,
+    n_nodes,
+    n_communities,
+    dim,
+):
+    """
+    Calculate the theoretical results.
+
+    Parameters
+    ----------
+    y_0: scipy.sparse.csc_matrix
+        Initial characteristic vector.
+    focal_node: int
+        Focal node.
+    membership: numpy.ndarray
+        Community assignment of each node.
+    coherence: float
+        Coherence parameter.
+    noise: float
+        Noise parameter.
+    com_com_rotation_matrix: dict
+        Rotation matrix for each pair of communities.
+    n_nodes: int
+        Number of nodes.
+    n_communities: int
+        Number of communities.
+    dim: int
+        Dimension of the matrix-weighted SBM.
+
+    Returns
+    -------
+    y_star: list
+        Theoretical results.
+    """
+    focal_node = 0
+    focal_community = membership[focal_node]
+
+    mat = y_0.toarray().reshape(n_nodes, dim)
+    y_0_bar = np.sum(
+        [
+            assign_rotation_matrix(
+                src=focal_node,
+                trg=k,
+                membership=membership,
+                com_com_rotation_matrix=com_com_rotation_matrix,
+                coherence=coherence,
+                noise=noise,
+                dim=dim,
+            ).dot(mat[k, :])
+            for k in range(n_nodes)
+        ],
+        axis=0,
+    )
+    y_star = []
+    for c in range(n_communities):
+        y_star.append(
+            com_com_rotation_matrix[(focal_community, c)].T.dot(y_0_bar) / n_nodes
+        )
+    return y_star
